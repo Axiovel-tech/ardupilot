b48939897f93575975f113eacd47c24fb245b5df
Marc Espuña <marc.espuna@axiovel.com>
2026-07-29 11:37:26 +0200

    AP_VisualOdom, EKF3: fuse per-axis extnav covariance

--- libraries/AP_NavEKF3/AP_NavEKF3_Measurements.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_Measurements.cpp
@@ -1080,11 +1080,11 @@ void NavEKF3_core::writeDefaultAirSpeed(float airspeed, float uncertainty)
 *            External Navigation Measurements           *
 ********************************************************/
 
-void NavEKF3_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, float posErr, float posErrZ, float angErr, uint32_t timeStamp_ms, uint16_t delay_ms, uint32_t resetTime_ms)
+void NavEKF3_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, const ExtNavPositionError &posErr, float angErr, uint32_t timeStamp_ms, uint16_t delay_ms, uint32_t resetTime_ms)
 {
 #if EK3_FEATURE_EXTERNAL_NAV
     // protect against NaN
-    if (pos.is_nan() || isnan(posErr) || isnan(posErrZ)) {
+    if (pos.is_nan() || posErr.axis.is_nan()) {
         return;
     }
 
@@ -1106,8 +1106,7 @@ void NavEKF3_core::writeExtNavData(const Vector3f &pos, const Quaternion &quat, 
     }
 
     extNavDataNew.pos = pos.toftype();
-    extNavDataNew.posErr = posErr;
-    extNavDataNew.posErrZ = posErrZ;
+    extNavDataNew.posErr = posErr.axis.toftype();
 
     // calculate timestamp
     timeStamp_ms = timeStamp_ms - delay_ms;
--- libraries/AP_NavEKF3/AP_NavEKF3_core.h
+++ libraries/AP_NavEKF3/AP_NavEKF3_core.h
@@ -341,7 +341,7 @@ public:
     * resetTime_ms : system time of the last position reset request (mSec)
     *
     */
-    void writeExtNavData(const Vector3f &pos, const Quaternion &quat, float posErr, float posErrZ, float angErr, uint32_t timeStamp_ms, uint16_t delay_ms, uint32_t resetTime_ms);
+    void writeExtNavData(const Vector3f &pos, const Quaternion &quat, const ExtNavPositionError &posErr, float angErr, uint32_t timeStamp_ms, uint16_t delay_ms, uint32_t resetTime_ms);
 
     /*
      * Write velocity data from an external navigation system
@@ -666,8 +666,7 @@ private:
 
     struct ext_nav_elements : EKF_obs_element_t {
         Vector3F        pos;        // XYZ position measured in a RH navigation frame (m)
-        ftype           posErr;     // horizontal position measurement error 1-std (m)
-        ftype           posErrZ;    // vertical position measurement error 1-std (m)
+        Vector3F        posErr;     // NED position measurement error 1-std (m)
         bool            posReset;   // true when the position measurement has been reset
         bool            corrected;  // true when the position has been corrected for sensor position
     };
